699655dfb2d66c556c658f59d01abe833ca7840e
Maxim Lukovskiy <lostlonginus9sp@gmail.com>
2023-11-07 02:46:14 +0200

    fixed add categories button; small improvements which i forgot

--- wallet/views.py
+++ wallet/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_protect
 
 from .forms import CategoryCreateForm
@@ -10,18 +10,14 @@ from django.contrib.auth.models import User
 @login_required
 def category_create_view(request):
     if request.method == 'POST':
+        
         form = CategoryCreateForm(request.POST, request=request)
         if form.is_valid():
             form.save()
-
-            # Обновляем контекст шаблона
-            context = {
-                'form': CategoryCreateForm(),
-            }
-            return render(request, 'homepage.html', context)
+            return redirect('home') 
     else:
         form = CategoryCreateForm()
-
+    
     context = {
         'form': form,
     }
